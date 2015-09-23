8198f9a78c9921b43d8b3cf0882d019d485035b6
shiyuzhang <shiyu.zhang@gmail.com>
2015-09-23 15:49:09 -0400

    Update Q1_Q10.py

--- Q1_Q10.py
+++ Q1_Q10.py
@@ -8,6 +8,15 @@ def generate_prime_factors (number):
             number /= n
         n += 1
     return prime_factors
+    
+#http://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n/3035188#3035188
+def primes(n):
+    """ Returns  a list of primes < n """
+    sieve = [True] * n
+    for i in xrange(3,int(n**0.5)+1,2):
+        if sieve[i]:
+            sieve[i*i::2*i]=[False]*((n-i*i-1)/(2*i)+1)
+    return [2] + [i for i in xrange(3,n,2) if sieve[i]]
 
 
 '''
